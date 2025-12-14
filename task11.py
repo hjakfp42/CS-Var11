a385fc40c3d496054e8ee389aa8d4e48d956ee57
hfjkahkjfhjka <130237953+hjakfp42@users.noreply.github.com>
2025-12-14 15:38:37 +0300

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,7 @@
+from math import ceil, log2
+
+bitsPerChar = ceil(log2(300 + 10))
+bytesPerID = ceil(bitsPerChar * 404 / 8)
+totalKB = bytesPerID * 3072 // 1024
+
+print(totalKB)
---
+++ task13.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+net = ip_network("23.140.159.160/255.255.252.0", 0)
+
+cnt = 0
+for ip in net:
+    ipb = f"{ip:b}"
+    if ipb[:16].count("1") >= ipb[16:].count("1"):
+        cnt += 1
+
+print(cnt)
+
